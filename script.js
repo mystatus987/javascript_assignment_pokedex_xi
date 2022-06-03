fdcfe77baeaee473f33a72d0ec39a73a29cd9b25
mystatus987 <101029763+mystatus987@users.noreply.github.com>
2022-06-03 16:06:27 +1000

    remove comment

--- script.js
+++ script.js
@@ -5,18 +5,12 @@ console.log(listOfPokemon);
 let arrayOfPokemons = [];
 
 const fetchPokemon = () => {
-    // create arru list of promise
     const promises = [];
-    // for (let i = 1; i <= 898; i++)
     for (let i = 1; i <= 300; i++) {
-        // get api url 
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        //fetch url and passing to parameter fetch then use call back function
-        // each request, promises to push 
         promises.push(fetch(url).then((response) => response.json()));
     }
 
-    // all promise an asynchronous operation 
     Promise.all(promises)
         .then(results => {
             const pokemon = results.map((data) => ({
@@ -25,44 +19,19 @@ const fetchPokemon = () => {
                 image: data.sprites['front_default'],
                 type: data.types.map((type) => type.type.name).join(", ")
             }));
-            // push array objects to global variable => arrayOfPokemon
             pokemon.map(pmn => arrayOfPokemons.push(pmn));
-            //display all pokemon 
             dispayPokemon(pokemon);
         });
-
-    /*
-     .then(data => {
-                // check data if the pokemon data is able to fetch
-                console.log(data);
-                // create arry of pokemon obj details
-                const pokemon = {
-                    name: data.name,
-                    id: data.id,
-                    image: data.sprites['front_default'],
-                    type: data.types.map((type) => type.type.name).join(", ")
-                };
-                console.log(pokemon);
-
-    testing 
-      // get what json data need from api
-      pokemon['name'] = data.name;
-      pokemon['id'] = data.id;
-      pokemon['image'] = data.sprites['front_default'];
-      // using map to get array and once get arry obj change these to string using join
-      pokemon['type'] = data.types.map((type) => type.type.name).join(", ")
-    */
 };
 
 fetchPokemon();
-// console.log(arrayOfPokemons);
 
 var count = 0;
 
 const addPokemonList = (pokemonId) => {
-    // search in global array for passed in id
+
     const selectedPokemon = arrayOfPokemons.find(pokemon => pokemon.id === pokemonId);
-    // storedPokemonList.push(selectedPokemon);
+
     if (selectedPokemon == null) {
         alert("There is no pokemon add list");
     } else {
@@ -79,9 +48,9 @@ const addPokemonList = (pokemonId) => {
 }
 
 const dispayPokemon = (pokemon) => {
-    // console.log(pokemon);
+
     const showPokemonHTMLString = pokemon.map(pokemons =>
-            `
+        `
             <div class ="container">
                 <ol class = "card" >
                     <img class = "card-image" src="${pokemons.image}"/>
@@ -91,7 +60,6 @@ const dispayPokemon = (pokemon) => {
                 </ol>
             </div>         
     `).join('')
-        // binding list of pokemon to html
-    listOfPokemon.innerHTML = showPokemonHTMLString;
 
+    listOfPokemon.innerHTML = showPokemonHTMLString;
 };
